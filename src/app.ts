31279f7966587faf6e104526574668e9760f82ae
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-07-31 19:08:43 -0300

    feat: added the cors in the application

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
+import cors from "cors";
 import express from "express";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Refund");
 });
-
 export { app };
