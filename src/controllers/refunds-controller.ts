cd072f5712a2e755edd1ae44c4c17656d099fcf4
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-09-11 12:13:09 -0300

    feat: created the functionality that inserts a refund within the application

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,8 +1,43 @@
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 import { Request, Response } from "express";
+import { z } from "zod";
+const CategoriesEnum = z.enum([
+  "food",
+  "others",
+  "services",
+  "transport",
+  "accommodation",
+]);
+
 class RefundsController {
   async create(request: Request, response: Response) {
-    response.json({ message: "Refund ok" });
+    const bodySchema = z.object({
+      name: z
+        .string()
+        .trim()
+        .min(1, { message: "Deve conter pelo menos 3 caracteres" }),
+      category: CategoriesEnum,
+      amount: z.number().positive({ message: "O valor precisa ser positivo" }),
+      filename: z.string().min(20, "Deve conter a imagem do comprovante"),
+    });
+    const { name, category, amount, filename } = bodySchema.parse(request.body);
+
+    if (!request.user?.id) {
+      throw new AppError("Unauthorized", 401);
+    }
+
+    const refund = await prisma.refunds.create({
+      data: {
+        name,
+        amount,
+        category,
+        filename,
+        userId: request.user.id,
+      },
+    });
+
+    response.status(201).json(refund);
   }
 }
-
 export { RefundsController };
