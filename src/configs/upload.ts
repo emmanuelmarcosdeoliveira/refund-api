f26183747944ab4557abcae097d44c5e141fb538
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-09-16 11:04:42 -0300

    feat: implement file upload functionality with validation and disk storage management

--- src/configs/upload.ts
+++ src/configs/upload.ts
@@ -3,23 +3,29 @@ import crypto from "node:crypto";
 import path from "node:path";
 
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
-const UPLOADS_FOLDER = path.resolve(__dirname, "..", "..", "uploads");
-
-const MAX_FILE_SIZE = 1024 * 1024 * 3; //3MB
-const ACCEPTED_IMAGES_TYPES = ["image/jpeg", "image/jpg", "image/png"];
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
+const MAX_SIZE = 3; //3MB
+const MAX_FILE_SIZE = 1024 * 1024 * MAX_SIZE;
+const ACCEPTED_IMAGES_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "application/pdf",
+];
 
 const MULTER = {
   storage: multer.diskStorage({
     destination: TMP_FOLDER,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString("hex");
-      const fileName = `${fileHash}- ${file.originalname}`;
+      const fileName = `${fileHash}-${file.originalname}`;
       return callback(null, fileName);
     },
   }),
 };
 
 export default {
+  MAX_SIZE,
   ACCEPTED_IMAGES_TYPES,
   MAX_FILE_SIZE,
   MULTER,
--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,8 +1,47 @@
+import uploadConfig from "@/configs/upload";
+import { DiskStorage } from "@/providers/disk-storage";
+import { AppError } from "@/utils/AppError";
 import { Request, Response } from "express";
+import z, { ZodError } from "zod";
 
 class UploadsController {
   async create(request: Request, response: Response) {
-    response.json("Ok uploads ");
+    const diskStorage = new DiskStorage();
+    try {
+      const fileSchema = z
+        .object({
+          filename: z.string().min(1, { message: "Arquivo é obrigatório" }),
+          mimetype: z
+            .string()
+            .refine(
+              (type) => uploadConfig.ACCEPTED_IMAGES_TYPES.includes(type),
+              {
+                message: `formato de arquivos permitidos: 
+              ${uploadConfig.ACCEPTED_IMAGES_TYPES}`,
+              }
+            ),
+          size: z
+            .number()
+            .positive()
+            .refine(
+              (size) => size <= uploadConfig.MAX_FILE_SIZE,
+              `Arquivo excede o tamanho de: ${uploadConfig.MAX_SIZE}`
+            ),
+        })
+        .passthrough();
+
+      const file = fileSchema.parse(request.file);
+      const filename = await diskStorage.saveFile(file.filename);
+
+      response.json({ filename });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        if (request.file) {
+          await diskStorage.deleteFile(request.file.filename, "tmp");
+        }
+        throw new AppError(error.issues[0].message);
+      }
+    }
   }
 }
 
