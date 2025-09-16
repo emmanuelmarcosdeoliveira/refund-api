54aad7703a73bda83464d813196d5d3908cf52a9
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-09-16 11:13:22 -0300

    feat: configure static file serving for uploads and add sample receipt image

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
+import uploadConfig from "@/configs/upload";
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import { errorHandling } from "./middleware/error-handling";
 import { routes } from "./routes";
-
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 app.use(errorHandling);
