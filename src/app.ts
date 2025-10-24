ccd34b8fc14af206258a64a9ce2c1f338584d2f1
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-10-24 00:16:25 -0300

    refactor:uploadsRoutes

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,13 @@ import "express-async-errors";
 import { errorHandling } from "./middleware/error-handling";
 import { routes } from "./routes";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://refund-ofs.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER));
